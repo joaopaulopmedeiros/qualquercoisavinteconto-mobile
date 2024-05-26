4231756e602e10f93ee71538f540ea5f9003f87d
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-05-26 13:07:41 -0300

    style: sign up

--- lib/constants/forms.dart
+++ lib/constants/forms.dart
@@ -1,4 +1,8 @@
+const nameHint = 'Fulano da Silva';
+const nameTitle = 'Nome';
+
 const emailHint = 'tester@admin.com';
 const emailTitle = 'Email';
+
 const passwordHint = '******';
 const passwordTitle = 'Senha';
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,4 +1,14 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:qualquercoisavinteconto/constants/colors.dart';
+import 'package:qualquercoisavinteconto/constants/fonts.dart';
+import 'package:qualquercoisavinteconto/constants/forms.dart';
+import 'package:qualquercoisavinteconto/constants/routes.dart';
+import 'package:qualquercoisavinteconto/widgets/app_logo_widget.dart';
+import 'package:qualquercoisavinteconto/widgets/background_widget.dart';
+import 'package:qualquercoisavinteconto/widgets/custom_button.dart';
+import 'package:qualquercoisavinteconto/widgets/custom_text_field.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -10,6 +20,45 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return backgroundWidget(
+        child: Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            (context.screenHeight * 0.1).heightBox,
+            appLogoWidget(),
+            10.heightBox,
+            "Primeiro Acesso"
+                .text
+                .fontFamily(bold)
+                .white
+                .size(18)
+                .make(),
+            12.heightBox,
+            Column(
+              children: [
+                customTextField(hint: nameHint, title: nameTitle),
+                customTextField(hint: emailHint, title: emailTitle),
+                customTextField(hint: passwordHint, title: passwordTitle),
+                customButton(
+                        color: redColor,
+                        title: "Finalizar",
+                        textColor: whiteColor,
+                        onPressed: () {})
+                    .box
+                    .width(context.screenWidth - 50)
+                    .make(),                
+              ],
+            )
+                .box
+                .white
+                .rounded
+                .padding(const EdgeInsets.all(16))
+                .width(context.screenWidth - 50)
+                .make()
+          ],
+        ),
+      ),
+    ));
   }
 }
