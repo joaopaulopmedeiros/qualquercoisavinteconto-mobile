1ffe04f1b3ab894334ddd19caa3f232c2bae9549
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-05-26 19:04:32 -0300

    style: basic address page style

--- lib/screens/manage_address_screen.dart
+++ lib/screens/manage_address_screen.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:qualquercoisavinteconto/constants/colors.dart';
 
 class ManageAddressScreen extends StatelessWidget {
   const ManageAddressScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: primaryColor,
+        title: const Text(
+          "Endereços",
+          style: TextStyle(
+              color: whiteColor, fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        iconTheme: const IconThemeData(color: whiteColor),
+      ),
+      body: Container(
+        color: Colors.grey[200],
+        child: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("opa")
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
