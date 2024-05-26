6b78620bd5d3f77da0e0143517f83d38283a57f4
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-05-26 17:52:41 -0300

    style: product details

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:qualquercoisavinteconto/constants/colors.dart';
 import 'package:qualquercoisavinteconto/models/product.dart';
@@ -12,29 +11,35 @@ class ProductDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(product.name, style: const TextStyle(
-                color: whiteColor, fontSize: 18, fontWeight: FontWeight.bold))               
+        backgroundColor: primaryColor,
+        title: Text(
+          product.name,
+          style: const TextStyle(color: whiteColor, fontSize: 18, fontWeight: FontWeight.bold),
+        ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              product.name,
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              'Price: \$${product.price}',
-              style: const TextStyle(fontSize: 18, color: Colors.grey),
-            ),
-            const SizedBox(height: 20),
-            Image.network(
-              product.imageUrl,
-              height: 200,
-              width: 200,
-            ),
-          ],
+      body: Container(
+        color: backgroundGrey,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                product.name,
+                style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Price: \$${product.price}',
+                style: const TextStyle(fontSize: 18, color: Colors.grey),
+              ),
+              const SizedBox(height: 20),
+              Image.network(
+                product.imageUrl,
+                height: 400,
+                width: 500,
+              ),
+            ],
+          ),
         ),
       ),
     );
