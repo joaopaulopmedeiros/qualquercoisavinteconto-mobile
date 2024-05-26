10cb6d54339148f0d76dcfbd080d8bb04f58d6b9
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-05-26 20:26:30 -0300

    feat: comeback

--- lib/screens/manage_address_screen.dart
+++ lib/screens/manage_address_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:qualquercoisavinteconto/constants/colors.dart';
 import 'package:qualquercoisavinteconto/constants/http.dart';
+import 'package:qualquercoisavinteconto/constants/routes.dart';
 import 'package:qualquercoisavinteconto/models/address.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -89,6 +90,7 @@ class _ManageAddressScreenState extends State<ManageAddressScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: primaryColor,
         title: const Text(
           "Endereços",
@@ -103,28 +105,39 @@ class _ManageAddressScreenState extends State<ManageAddressScreen> {
       body: Container(
         color: Colors.grey[200],
         child: ListView.builder(
-          itemCount: addresses.length,
-          itemBuilder: (ctx, i) => ListTile(
-            title: Text(addresses[i].street),
-            subtitle: Text(addresses[i].number),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.edit),
-                  onPressed: () {
-                   editAddress(addresses[i]);
-                  },
+          itemCount: addresses.length + 1,
+          itemBuilder: (ctx, i) {
+            if (i == addresses.length) {
+              return ListTile(
+                title: const Text("Voltar para Home"),
+                onTap: () {
+                  Navigator.pushNamed(context, homeRoute);
+                },
+              );
+            } else {
+              return ListTile(
+                title: Text(addresses[i].street),
+                subtitle: Text(addresses[i].number),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.edit),
+                      onPressed: () {
+                        editAddress(addresses[i]);
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {
+                        deleteAddress(addresses[i].id);
+                      },
+                    ),
+                  ],
                 ),
-                IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () {
-                    deleteAddress(addresses[i].id);
-                  },
-                ),
-              ],
-            ),
-          ),
+              );
+            }
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -137,7 +150,10 @@ class _ManageAddressScreenState extends State<ManageAddressScreen> {
           );
         },
         backgroundColor: primaryColor,
-        child: const Icon(Icons.add, color: Colors.white,),
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
       ),
     );
   }
