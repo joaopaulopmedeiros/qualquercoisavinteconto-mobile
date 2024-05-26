2ccc0cd83b7de7bf66c62eb498bb401edb56056b
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-05-26 19:18:25 -0300

    feat: list addresses

---
+++ lib/models/address.dart
@@ -0,0 +1,37 @@
+import 'dart:ffi';
+
+class Address {
+  final int id;
+  final String street;
+  final String number;
+  final String city;
+  final String state;
+
+  Address({
+    required this.id,
+    required this.street,
+    required this.number,
+    required this.city,
+    required this.state,
+  });
+
+  factory Address.fromJson(Map<String, dynamic> json) {
+    return Address(
+      id: json['id'] ?? 0,
+      street: json['street'] ?? '',
+      number: json['number'] ?? '',
+      city: json['city'] ?? '',
+      state: json['state'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'street': street,
+      'number': number,
+      'city': city,
+      'state': state,
+    };
+  }
+}
--- lib/screens/manage_address_screen.dart
+++ lib/screens/manage_address_screen.dart
@@ -1,29 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:qualquercoisavinteconto/constants/colors.dart';
+import 'package:qualquercoisavinteconto/models/address.dart'; // Import the Address model
 
 class ManageAddressScreen extends StatelessWidget {
   const ManageAddressScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final List<Address> addresses = List.generate(
+      5,
+      (index) => Address(
+        id: index,
+        street: 'Street $index',
+        number: '$index',
+        city: 'City $index',
+        state: 'State $index',
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: primaryColor,
         title: const Text(
           "Endereços",
           style: TextStyle(
-              color: whiteColor, fontSize: 18, fontWeight: FontWeight.bold),
+            color: whiteColor,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
         ),
         iconTheme: const IconThemeData(color: whiteColor),
       ),
       body: Container(
         color: Colors.grey[200],
-        child: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("opa")
-            ],
+        child: ListView.builder(
+          itemCount: addresses.length,
+          itemBuilder: (ctx, i) => ListTile(
+            title: Text(addresses[i].street),
+            subtitle: Text('\$${addresses[i].number}'),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.edit),
+                  onPressed: () {
+                    // Add edit functionality here
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () {
+                    // Add delete functionality here
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
