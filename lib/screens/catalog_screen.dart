184d7de701f0c96f5564397e88f15d1c24883538
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-05-26 15:53:33 -0300

    feat: get current user name

--- lib/screens/catalog_screen.dart
+++ lib/screens/catalog_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qualquercoisavinteconto/providers/auth_provider.dart';
 
 class CatalogScreen extends StatelessWidget {
   const CatalogScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final provider = Provider.of<AuthProvider>(context);
+    return Column(children: [
+      Text("Olá ${provider.getCurrentUser()?.name}")
+    ],);
   }
 }
