e23605a6bd75ce1c0731ba077ae8edf6bced1319
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-05-26 21:33:06 -0300

    feat: handle error

--- lib/providers/catalog_provider.dart
+++ lib/providers/catalog_provider.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:qualquercoisavinteconto/constants/http.dart';
 import 'package:qualquercoisavinteconto/models/product.dart';
+import 'package:qualquercoisavinteconto/utils/http.dart';
 import 'auth_provider.dart';
 
 class CatalogProvider with ChangeNotifier {
@@ -24,9 +25,13 @@ class CatalogProvider with ChangeNotifier {
       },
     );
 
-    if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body);
-      _products = data.map((product) => Product.fromJson(product)).toList();
+    if (isSuccessful(response)) {
+      if (response.body.isEmpty) {
+        _products = [];
+      } else {
+        final List<dynamic> data = json.decode(response.body);
+        _products = data.map((product) => Product.fromJson(product)).toList();
+      }
       notifyListeners();
     } else {
       throw Exception('Failed to load products');
